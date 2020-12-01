46cba46641cd614ca8df49ade0012392398fcfa4
code1031 <wangzijie1031@qq.com>
2020-12-01 09:22:04 +0800

    拆分独立路由模块

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const app = express()
+const loginRouter = require(path.join(__dirname, 'routers/login-router.js'))
 
 // 配置跨域
 app.use(cors())
@@ -14,6 +16,11 @@ app.use(express.urlencoded({ extended: true }))
 app.listen(8888, () => {
   console.log('running...')
 })
+
+// 配置路由模块 /api/abc
+// app.use函数的参数一表示在路由的前面统一添加一层路径
+// app.use函数的参数二表示独立的路由模块
+app.use('/api', loginRouter)
 
 app.get('/data', (req, res) => {
   res.send('hello')
