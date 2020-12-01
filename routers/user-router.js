067347181bc7db2ec4eaafa37fd4397e52aec58e
code1031 <wangzijie1031@qq.com>
2020-12-01 16:55:51 +0800

    更新头像

--- routers/user-router.js
+++ routers/user-router.js
@@ -2,6 +2,7 @@
   用户模块
 */
 const express = require('express')
+const utility = require('utility')
 const path = require('path')
 const router = express.Router()
 const db = require(path.join(__dirname, '../common.js'))
@@ -26,6 +27,53 @@ router.get('/userinfo', async (req, res) => {
     res.json({
       status: 1,
       message: '查询用户信息失败'
+    })
+  }
+})
+
+// 更新密码
+router.post('/updatepwd', async (req, res) => {
+  // 1、获取参数
+  let id = req.user.id
+  let params = req.body
+  params.oldPwd = utility.md5(params.oldPwd)
+  params.newPwd = utility.md5(params.newPwd)
+  // 2、操作数据库
+  let sql = 'update myuser set password = ? where id = ? and password = ?'
+  let ret = await db.operateDb(sql, [params.newPwd, id, params.oldPwd])
+  // 3、响应状态
+  if (ret && ret.affectedRows > 0) {
+    res.json({
+      status: 0,
+      message: '重置密码成功'
+    })
+  } else {
+    res.json({
+      status: 1,
+      message: '重置密码失败'
+    })
+  }
+})
+
+// 更新头像
+router.post('/update/avatar', async (req, res) => {
+  // 1、获取参数
+  let id = req.user.id
+  let params = req.body
+  console.log(params)
+  // 2、操作数据库
+  let sql = 'update myuser set user_pic = ? where id = ?'
+  let ret = await db.operateDb(sql, [params.avatar, id])
+  // 3、响应状态
+  if (ret && ret.affectedRows > 0) {
+    res.json({
+      status: 0,
+      message: '更新头像成功'
+    })
+  } else {
+    res.json({
+      status: 1,
+      message: '更新头像失败'
     })
   }
 })
