f7bdcb05e6f9a0dcbea4a2c2b10a06572ab231fe
code1031 <wangzijie1031@qq.com>
2020-12-01 09:10:26 +0800

    完善入口文件功能

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+// 配置跨域
+app.use(cors())
+
+// 处理客户端请求post参数
+// for parsing application/json
+app.use(express.json()) 
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })) 
+
+app.listen(8888, () => {
+  console.log('running...')
+})
+
+app.get('/data', (req, res) => {
+  res.send('hello')
+})
